33cd7d5a827fb4f9f4e19f7a0fd05c2a6f59e64a
Heudiard Eric <eric.heudiard@maps-system.com>
2023-07-04 10:50:09 +0200

    Exercise 1 done

--- src/TodoListBasic/TodoListBasic.tsx
+++ src/TodoListBasic/TodoListBasic.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './TodoListBasic.css';
 
 const TodoListBasic = () => {
-    return <div>TodoListBasic</div>;
+    const [tasks, setTasks] = useState<any>({
+        todo: {label: 'To do', values: []},
+        progress: {label: 'Progress', values: []},
+        done: {label: 'Done', values: []}
+    });
+
+    const [task, setTask] = useState<any>('');
+    const [taskType, setTaskType] = useState<any>('todo');
+
+    const handleTaskLabelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTask(event.target.value);
+    };
+
+    const handleTaskTypeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setTaskType(event.target.value);
+    };
+
+    const handleAddTask = (event: React.FormEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+
+        if (tasks[taskType].values.indexOf(task) === -1) {
+            tasks[taskType].values.push(task);
+            setTasks((prevState: any) => ({...prevState, [taskType]: tasks[taskType]}));
+        }
+    };
+
+    return <main>
+        <div id="inputs-wrapper">
+            <input onChange={handleTaskLabelChange}/>
+            <select onChange={handleTaskTypeChange}>
+                {Object.keys(tasks).map(taskType => {
+                    return (
+                        <option value={taskType}>{tasks[taskType].label}</option>
+                    );
+                })}
+            </select>
+            <button onClick={handleAddTask}>Add task</button>
+        </div>
+        <div id="data-wrapper">
+            {Object.keys(tasks).map(taskType => {
+                return (
+                    <div>
+                        <h2>{tasks[taskType].label}</h2>
+                        <ul>
+                            {tasks[taskType].values.map((task: any) =>
+                                <li>{task}</li>
+                            )}
+                        </ul>
+                    </div>
+                );
+            })}
+        </div>
+    </main>;
 };
 
 export default TodoListBasic;
