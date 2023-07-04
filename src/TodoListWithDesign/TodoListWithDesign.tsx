c71f79561923e0393dd580c0dde738136e005577
Heudiard Eric <eric.heudiard@maps-system.com>
2023-07-04 14:11:39 +0200

    Exercise 2 - Done

--- src/TodoListWithDesign/TodoListWithDesign.tsx
+++ src/TodoListWithDesign/TodoListWithDesign.tsx
@@ -6,12 +6,32 @@ const { Title } = Typography;
 const TodoListWithDesign = () => {
     const [tasks, setTasks] = useState<any>({
         todo: {label: 'To do', values: []},
-        progress: {label: 'Progress', values: []},
+        progress: {label: 'In progress', values: []},
         done: {label: 'Done', values: []}
     });
 
+    const [column, setColumn] = useState<any>('');
     const [task, setTask] = useState<any>('');
     const [taskType, setTaskType] = useState<any>('todo');
+
+    const handleColumnLabelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setColumn(event.target.value);
+    };
+
+    const handleAddColumn = (event: any) => {
+        event.preventDefault();
+
+        let columnId = column.replace(' ', '').toLowerCase();
+
+        if (columnId !== '' && !tasks[columnId]) {
+            tasks[columnId] = {
+                label: column,
+                values: []
+            };
+            setTasks((prevState: any) => ({...prevState, [columnId]: tasks[columnId]}));
+            setColumn('');
+        }
+    };
 
     const handleTaskLabelChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setTask(event.target.value);
@@ -27,13 +47,22 @@ const TodoListWithDesign = () => {
         if (task !== '' && tasks[taskType].values.indexOf(task) === -1) {
             tasks[taskType].values.push(task);
             setTasks((prevState: any) => ({...prevState, [taskType]: tasks[taskType]}));
+            setTask('');
         }
     };
 
     return <main>
         <Row gutter={16}>
+            <Col className={'gutter-row'} span={20}>
+                <Input onChange={handleColumnLabelChange} value={column}/>
+            </Col>
+            <Col className={'gutter-row'} span={4}>
+                <Button type="primary" onClick={handleAddColumn} style={{ width: '100%' }}>Add column</Button>
+            </Col>
+        </Row>
+        <Row gutter={16} style={{ marginTop: '20px' }}>
             <Col className={'gutter-row'} span={12}>
-                <Input onChange={handleTaskLabelChange}/>
+                <Input onChange={handleTaskLabelChange} value={task}/>
             </Col>
             <Col className={'gutter-row'} span={8}>
                 <Select onChange={handleTaskTypeChange} style={{ width: '100%' }}>
@@ -56,8 +85,8 @@ const TodoListWithDesign = () => {
                             header={<Title level={4} style={{ textAlign: 'center' }}>{tasks[taskType].label}</Title>}
                             bordered
                         >
-                            {tasks[taskType].values.map((task: any) =>
-                                <List.Item>{task}</List.Item>
+                            {tasks[taskType].values.map((taskValue: any) =>
+                                <List.Item>{taskValue}</List.Item>
                             )}
                         </List>
                     </Col>
